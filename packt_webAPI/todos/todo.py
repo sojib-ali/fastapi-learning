04d29fd5ac88aa2c0750abde645e8f2ab6875eba
sojib <sojib.ali@hotmail.com>
2025-08-07 18:56:43 +0600

    completed path parameter

--- packt_webAPI/todos/todo.py
+++ packt_webAPI/todos/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from todos.model import Todo
 
 todo_router = APIRouter()
@@ -13,3 +13,15 @@ async def add_todo(todo:Todo) -> dict:
 @todo_router.get('/todo')
 async def retrieve_todos() -> dict:
     return{'todos': todo_list}
+
+@todo_router.get('/todo/{todo_id}')
+async def get_single_todo(todo_id: int) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                'todo': todo
+            }
+    return {
+        'message': "Todo with supplied ID doesn't exist."
+    }
+
