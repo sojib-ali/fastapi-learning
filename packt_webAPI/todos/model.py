77815e8b772f64b50003c5918ea5ebe6b38dfb10
sojib-ali <sojib.ali@hotmail.com>
2025-08-11 18:45:25 +0600

    implemented a CRUD todos with fastapi

--- packt_webAPI/todos/model.py
+++ packt_webAPI/todos/model.py
@@ -1,5 +1,16 @@
 from pydantic import BaseModel
 
-class Todo (BaseModel):
+class Todo(BaseModel):
     id: int
     item: str
+
+class TodoItem(BaseModel):
+    item: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+    }
