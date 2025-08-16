ed8b072a14da592618f3ec7afabd57de20bf716b
sojib-ali <sojib.ali@hotmail.com>
2025-08-16 18:50:51 +0600

    added class Even table form SQLModel

--- packt_webAPI/planner/models/events.py
+++ packt_webAPI/planner/models/events.py
@@ -1,24 +1,25 @@
-from pydantic import BaseModel, Field
-from typing import Optional, List
+from sqlmodel import Field, SQLModel, JSON, Column
+from typing import List, Optional
 from uuid import UUID, uuid4
 
-class Event(BaseModel):
-    id: UUID = Field(default_factory = uuid4)
+
+class Event(SQLModel, table=True):
+    id: Optional[UUID] = Field(default_factory=uuid4, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
 
-    model_config ={
-        "json_schema_extra":{
-            "example":{
+    model_config = {
+        "arbitrary_types_allowed": True,
+        "json_schema_extra": {
+            "example": {
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
                 "description": "We will be discussing the content of the FastAPI book in this event.",
                 "tags": ["python", "fastapi", "book", "launch"],
                 "location": "Google Meet"
-                
             }
         }
     }
