12f90d2a162c86b4d26b4be439cef6fbae7a68f1
sojib-ali <sojib.ali@hotmail.com>
2025-08-17 12:09:34 +0600

    added update route with session to update into database directly

--- packt_webAPI/planner/routes/events.py
+++ packt_webAPI/planner/routes/events.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Body, HTTPException, status, Depends, Request
 from database.connection import get_session
 from  models.events import Event, EventUpdate
+from sqlmodel import Session, select
 from typing import List
 from uuid import UUID
 
@@ -9,14 +10,17 @@ event_router = APIRouter(tags=["Events"])
 events = []
 
 @event_router.get("/", response_model = List[Event])
-async def retrieve_all_events() -> List[Event]:
-    return events
+async def retrieve_all_events(session = Depends(get_session)) -> List[Event]:
+   statement = select(Event)
+   events = session.exec(statement).all()
+   return events
 
 @event_router.get("/{id}", response_model = Event)
-async def retrieve_event(id: UUID) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
+async def retrieve_event(id: UUID, session = Depends(get_session)) -> Event:
+
+    event = session.get(Event, id)
+    if event:
+        return event
     
     raise HTTPException(
         status_code = status.HTTP_404_NOT_FOUND,
@@ -32,6 +36,21 @@ async def create_event(new_event: Event,
     return {
         "message": "Event created successfully"
     }
+
+@event_router.put("/edit/{id}", response_model = Event)
+async def update_event(id: UUID, new_data: EventUpdate, session = Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.model_dump(exclude_unset = True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+
+        return event
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                        detail = "Event with supplied ID does not exist")
 
 @event_router.delete("/{id}")
 async def delete_event(id: UUID) -> dict:
