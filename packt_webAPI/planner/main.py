03924b23271b7444e977c17ca2cd6475cf9040cf
sojib-ali <sojib.ali@hotmail.com>
2025-08-17 11:26:04 +0600

    created database connection with the events

--- packt_webAPI/planner/main.py
+++ packt_webAPI/planner/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Code to run on startup
+    conn()
+    yield
+    # Code to run on shutdown
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
--- packt_webAPI/planner/models/events.py
+++ packt_webAPI/planner/models/events.py
@@ -23,3 +23,23 @@ class Event(SQLModel, table=True):
             }
         }
     }
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    model_config ={
+        "json_schema_extra": {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the content of the FastAPI book in this event.",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet" 
+            }
+        }
+    }
