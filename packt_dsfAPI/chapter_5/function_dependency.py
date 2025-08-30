9745ea213454b6f1d810800f151a3001cb338943
sojib-ali <sojib.ali@hotmail.com>
2025-08-30 11:35:29 +0600

    tested dependency function

---
+++ packt_dsfAPI/chapter_5/function_dependency.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, Depends, Query, HTTPException, status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+async def pagination(skip: int = 0, limit: int = 0) -> tuple[int, int]:
+    return (skip, limit)
+
+# complex dependecy function - 
+async def com_pagination(
+        skip: int = Query(0, ge=0),
+        limit: int = Query(10, ge = 0),
+) -> tuple [int, int]: 
+        capped_lmit = min(100, limit)
+        return (skip, capped_lmit)
+
+
+@app.get("/items")
+async def list_items(p: tuple[int, int] = Depends(pagination)):
+    skip, limit = p
+    return {"skip" : skip, "limit": limit}
+
+@app.get("/things")
+async def list_things(p: tuple[int, int] = Depends(pagination)):
+    skip, limit = p
+    return {"skip" : skip, "limit": limit}
+
+
+#getting an object or raising a 404 error
+class Post(BaseModel):
+     id: int
+     title: str
+     content: str
+
+class PostUpdate(BaseModel):
+     title: str | None = None
+     content: str | None = None
+
+class DummyDatabase:
+     posts: dict[int, Post] = {}
+
+db = DummyDatabase()
+
+db.posts = {
+     1: Post(id = 1, title="Post 1", content = "Content 1"),
+     2: Post(id = 2, title="Post 2", content="Content 2"),
+     3: Post(id = 3, title = "Post 3", content = "Content 3"),
+}
+
+async def get_post_or_404(id: int) -> Post:
+     try:
+          return db.posts[id]
+     except KeyError:
+          raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+
+@app.get("/posts/{id}")
+async def get(post: Post = Depends(get_post_or_404)):
+     return post
+
+@app.patch("/posts/{id}")
+async def update(post_update: PostUpdate, post: Post = Depends(get_post_or_404)):
+     update_data = post_update.model_dump(exclude_unset=True)
+     updated_post = post.model_copy(update=update_data)
+     db.posts[post.id] = updated_post
+     return updated_post
+
+@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+async def delete(post: Post = Depends(get_post_or_404)):
+    db.posts.pop(post.id)
+
+
+
+
+
+     
