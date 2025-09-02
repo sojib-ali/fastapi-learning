123fab5b14cddec58234d1e93b42af0d32e566d3
sojib-ali <sojib.ali@hotmail.com>
2025-09-02 17:07:58 +0600

    created models and schemas for comment and post relationships

--- packt_dsfAPI/chapter_6/b_sqlalchemy/models.py
+++ packt_dsfAPI/chapter_6/b_sqlalchemy/models.py
@@ -1,10 +1,21 @@
 from datetime import datetime
 
 from sqlalchemy.types import DateTime, Integer, String, Text
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key= True, autoincrement = True)
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), nullable = False)
+    publication_date: Mapped[datetime] = mapped_column(DateTime, nullable = False, default = datetime.now)
+    content: Mapped[str] = mapped_column(Text, nullable = False)
+    
+    post: Mapped["Post"] = relationship("Post", back_populates = "comments")
 
 class Post(Base):
     __tablename__ = "posts"
@@ -13,3 +24,5 @@ class Post(Base):
     publication_date: Mapped[datetime] = mapped_column(DateTime, nullable = False, default = datetime.now)
     title: Mapped[str] = mapped_column(String(255), nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable = False)
+
+    comments: Mapped[list[Comment]] = relationship("Comment", cascade = "all,  delete")
--- packt_dsfAPI/chapter_6/b_sqlalchemy/schemas.py
+++ packt_dsfAPI/chapter_6/b_sqlalchemy/schemas.py
@@ -2,6 +2,20 @@ from datetime import datetime
 
 from pydantic import BaseModel, Field
 
+class CommentBase(BaseModel):
+    publication_date: datetime = Field(default_factory=datetime.now)
+    content: str
+
+    class Config: 
+        orm_mode = True
+
+class CommentCreate(CommentBase):
+    pass
+
+class CommentRead(CommentBase):
+    id: int
+    post_id: int
+
 class PostBase(BaseModel):
     title: str
     content: str
@@ -19,3 +33,4 @@ class PostCreate(PostBase):
 
 class PostRead(PostBase):
     id: int
+    comments: list[CommentRead]
