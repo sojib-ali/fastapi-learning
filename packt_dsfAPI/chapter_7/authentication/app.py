60dea48ab85c71b22ee991f5b4e0d692cadb08b2
sojib-ali <sojib.ali@hotmail.com>
2025-09-07 18:24:48 +0600

    implemented registration routes with database

---
+++ packt_dsfAPI/chapter_7/authentication/app.py
@@ -0,0 +1,35 @@
+import contextlib
+from fastapi import FastAPI, status, Depends, HTTPException
+from authentication import schemas
+from database import create_all_tables, get_async_session
+from models import User
+from sqlalchemy.ext.asyncio import AsyncSession
+from password import get_password_hash
+from sqlalchemy import exc
+
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_all_tables()
+    yield
+
+app = FastAPI()
+
+@app.post(
+    "/register", status_code = status.HTTP_201_CREATED, response_model = schemas.UserRead
+)
+async def register(
+    user_create: schemas.UserCreate, session: AsyncSession = Depends(get_async_session)
+) -> User:
+    hashed_password = get_password_hash(user_create.password)
+    user = User(
+        **user_create.model_dump(exclude={"password"}), hashed_password = hashed_password
+    )
+
+    try:
+        session.add(user)
+        await session.commit()
+    except exc.IntegrityError:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST, detail = "Email alread exists"
+        )
+    return user
