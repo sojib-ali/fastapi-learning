341c7108c3707c143da69393dd0a8a44aeccfa44
sojib-ali <sojib.ali@hotmail.com>
2025-09-09 16:03:04 +0600

    completed adding csrf token on cookies

---
+++ packt_dsfAPI/chapter_7/cors/app_with_cors.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:9000"], 
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"], 
+    max_age = -1 # only for this project, remove this in real project
+
+)
+
+@app.get("/")
+async def get():
+    return {"detail": "GET response"}
+ 
+@app.post("/")
+async def post(request: Request):
+    json = await request.json()
+    return {"detail": "POST response", "input_payload": json}
